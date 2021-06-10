5807da4b94c0a66582153097f8ffb8427737a956
Shafee <rashidodd@gmail.com>
2021-06-10 23:54:40 +0600

    1. Change design 2. Icon added

--- app/src/main/java/com/example/payoneerpaymentapitask/adapters/ListResultRecyclerViewAdapter.java
+++ app/src/main/java/com/example/payoneerpaymentapitask/adapters/ListResultRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.payoneerpaymentapitask.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +19,7 @@ import java.util.List;
 
 public class ListResultRecyclerViewAdapter extends RecyclerView.Adapter<ListResultRecyclerViewAdapter.ListResultViewHolder> {
     private List<Applicable> mApplicableList = new ArrayList<>();
+    private int mCheckedPosition = -1;
 
     @NonNull
     @Override
@@ -32,6 +34,22 @@ public class ListResultRecyclerViewAdapter extends RecyclerView.Adapter<ListResu
         holder.mItemName.setText(applicableData.getLabel());
         String logoUrl = applicableData.getLinks().getLogo();
         Picasso.get().load(logoUrl).placeholder(R.drawable.circular_progress_animation).into(holder.mItemLogo);
+
+        holder.mCheckBox.setChecked(position == mCheckedPosition);
+        applicableData.setSelected(position == mCheckedPosition);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (position == mCheckedPosition) {
+                    holder.mCheckBox.setChecked(false);
+                    mCheckedPosition = -1;
+                } else {
+                    mCheckedPosition = position;
+                    notifyDataSetChanged();
+                }
+            }
+        });
     }
 
     @Override
@@ -47,12 +65,16 @@ public class ListResultRecyclerViewAdapter extends RecyclerView.Adapter<ListResu
     static class ListResultViewHolder extends RecyclerView.ViewHolder {
         private TextView mItemName;
         private ImageView mItemLogo;
+        private CheckBox mCheckBox;
+        private View mView;
 
         public ListResultViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            mView = itemView.findViewById(R.id.id_containerLayout_1);
             mItemName = itemView.findViewById(R.id.item_name);
             mItemLogo = itemView.findViewById(R.id.item_logo);
+            mCheckBox = itemView.findViewById(R.id.id_checkbox);
         }
     }
 }
